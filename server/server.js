4d0a0ba0d21adad7564f13302812527881b22dd4
Aadhithya S B <aadhithyasb1@gmail.com>
2024-08-07 03:01:25 +0530

    handle cors error

--- server/server.js
+++ server/server.js
@@ -11,7 +11,18 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 8800;
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://asb-blog-app.vercel.app", 
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+  optionsSuccessStatus: 200, 
+};
+app.use((req, res, next) => {
+  console.log("Request Origin:", req.headers.origin);
+  next();
+});
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
